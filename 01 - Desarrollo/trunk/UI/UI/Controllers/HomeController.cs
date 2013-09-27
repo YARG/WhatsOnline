e974eafa3055741ca239e0df41540ba5c2e73d08
jonathanconfenti90 <jonathanconfenti90@hotmail.com>
2013-09-27 17:40:38 -0300

    Primeras pruebas

--- 01 - Desarrollo/trunk/UI/UI/Controllers/HomeController.cs
+++ 01 - Desarrollo/trunk/UI/UI/Controllers/HomeController.cs
@@ -8,16 +8,49 @@ namespace UI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            //ViewBag.Message = "Bienvenido a Whats! :)";
+        //public ActionResult Index()
+        //{
+        //    //ViewBag.Message = "Bienvenido a Whats! :)";
 
-            return View();
-        }
+        //    ViewData["Mensaje"] = "";
+        //    return View();
+        //}
 
         public ActionResult About()
         {
             return View();
         }
+
+        public ActionResult Index(string id)
+        {
+            if (id != null)
+            {
+                string htmlUrl = "https://v.whatsapp.net/v2/exist?cc=54&in=" + id.Substring(2) + "&to=" + id + "&lg=es&lc=AR&mcc=204&mnc=008&method=voice";
+
+                using (var webClient = new System.Net.WebClient())
+                {
+                    var json = webClient.DownloadString(htmlUrl);
+                    if (json.Contains("created"))
+                    {
+                        ViewData["Mensaje"] = "Existe una cuenta para ese n&uacute;mero de tel&eacute;fono.";
+                    }
+                    else 
+                    {
+                        ViewData["Mensaje"] = "No existe una cuenta para ese n&uacute;mero de tel&eacute;fono.";
+                    }
+                }
+
+                
+
+                
+            }
+            else 
+            {
+                ViewData["Mensaje"] = "Por favor ingrese un valor v&aacute;lido";
+            }
+
+            return View();
+        }
+
     }
 }
--- 01 - Desarrollo/trunk/UI/UI/Views/Home/Index.aspx
+++ 01 - Desarrollo/trunk/UI/UI/Views/Home/Index.aspx
@@ -13,15 +13,19 @@
     <br />
     <table align="center" border="0">
         <tr>
-            <td><input type="text" ID="txtNumeroTelefono" placeholder="Numero de teléfono" /></td>
+            <td colspan="2"><span><% ViewData["Mensaje"].ToString(); %></span></td>
+        </tr>
+        <tr>
+            <td valign="top"><input type="text" ID="txtNumeroTelefono" runat="server" name="txtNumeroTelefono" class="text-box" style="height:32px;" placeholder="Numero de teléfono" /></td>
             <td>
-                <div class="alert-message success">
+                <div class="alert alert-success"  style="height:23px;">
                     <p>Ingrese el número con código de país y provincia. Ej: 541122438347</p>
                 </div>
             </td>
         </tr>
         <tr>
-            <td colspan="2" align="center"><input type="button" ID="btnAceptar" class="btn-success btn-large" Value="Consultar" /></td>
+            <%--<td colspan="2" align="center"><input type="button" ID="btnAceptar" class="btn-success btn-large" Value="Consultar" /></td>--%>
+            <td colspan="2" align="center"><%: Html.ActionLink("Consultar", "Index", "Home", new { id = Request["txtNumeroTelefono"] }, new { @class = "btn-success btn-large" })%></td>
         </tr>
     </table>
 </asp:Content>
